89dfa378e6290c34d54ee0127bed2c6bc3d7068f
Xavier OILLIC <xavieroillic@gmail.com>
2023-11-24 19:18:19 +0100

    finalisé ou presque

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const urlApiP2 = `https://reqres.in/api/users?page=2`;
 
 const body1Element = document.getElementById("body1"); 
 
-function nextOne (event) {
 
     fetch(urlApi).then((response) => {
         return response.json();
@@ -20,21 +19,30 @@ function nextOne (event) {
 
     <div class= "user_cards">
    <div id= "id">
-        <p>${jsonFramboise.data[zindex].id}</p>
+        <p>id : ${jsonFramboise.data[zindex].id}</p>
    </div>
    <div id= "avatar"><img src = "${jsonFramboise.data[zindex].avatar}" alt ="Picture of ${jsonFramboise.data[zindex].first_name} "/></div>
    <div id= "prenom_nom">
         <p>${jsonFramboise.data[zindex].first_name} ${jsonFramboise.data[zindex].last_name}</p>
    </div>
    <div id= "email">
-        <p>${jsonFramboise.data[zindex].email}</p>
+        <p>@ ${jsonFramboise.data[zindex].email}</p>
    </div>
    </div>
-    `;
-    
+    `;   
     
     }  // ===> FIN de FOR LOOP
 
 });  // ===> FIN de FETCH
+
+
+function nextOne (event) {
+    event.preventDefault();
+
+    const searchPage = document.getElementById("page_choice").value;
+console.log(searchPage);
+
 }
 nextOne(); // ==> FIN de FONCTION 
+
+
